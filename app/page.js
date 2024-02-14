543b85bc8abaebdd54a522cd8aa97ae07649f5d4
badalku27 <150800647+badalku27@users.noreply.github.com>
2024-02-15 02:53:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" BADAL "}
+            By{" BADAL"}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
